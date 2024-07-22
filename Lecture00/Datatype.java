edcfd2120352e4ccdbee1f5feaed13f745e8e605
G_puniya <60355338+p4puniya@users.noreply.github.com>
2024-07-22 22:45:06 +0530

    Datatype_Start

---
+++ Lecture00/Datatype.java
@@ -0,0 +1,19 @@
+package Lecture00;
+class Datatype{
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.print("Hi");
+    }
+}
+
+//Hello
+//HI
+//Comments
+/*askdjfh
+ * 
+ * akjdf
+ * MultiLine Comment
+ * sakjdf
+ * 
+ *
+ */
